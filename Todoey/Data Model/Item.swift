615c02c9f479fd7b0732fe706917b964b2133177
Amit Vaidya <amitvaidhya@gmail.com>
2018-06-05 19:35:57 +0530

    converted to MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Amit Vaidya on 05/06/18.
+//  Copyright © 2018 Tech Navigator. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
